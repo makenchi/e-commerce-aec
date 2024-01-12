ab38c3d13ae49d9243b010661446574f4f4e9647
Lucas Silva <lucas.silva@credisis.com.br>
2024-01-12 16:57:06 -0300

    feat: criando header e footer

---
+++ ecommerce-aec/src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react'
+
+const Footer = () => {
+  return (
+    <Fragment>
+      <div className='text-center bg-dark text-white py-2 fixed-bottom'>
+        <span>
+          Developed by{" "}
+          <a href='https://github.com/makenchi' target='blank'>
+            Lucas Santos
+          </a>
+        </span>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Footer
